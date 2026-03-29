65c62cf9e37b4320ea2b174d566889e4386201ac
Taher Seifi <taherseifi152@gmail.com>
2026-03-29 18:34:36 +0300

    add final dynamic sitemap and robots

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import prerender from '@prerenderer/rollup-plugin'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  build: {
-    rollupOptions: {
-      plugins: [
-        prerender({
-          routes: [
-            '/listings',
-            '/nearby-services',
-            '/faq',
-            '/contact-us',
-            '/privacy-policy',
-            '/submit-business',
-            '/guide-submit-business',
-          ],
-        }),
-      ],
-    },
-  },
 })
